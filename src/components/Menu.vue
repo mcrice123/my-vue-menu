556eb8b920b0888db25a22423959101ea82ccc11
Maria Rice <maria@lifeboatcreative.com>
2020-05-11 11:19:41 -0500

    Initialize new project with some custom components

---
+++ src/components/Menu.vue
@@ -0,0 +1,65 @@
+<template>
+    <section class="hero is-info is-large">
+        <top-nav :title="title"></top-nav>
+
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <p class="title">
+                    {{ title }}
+                </p>
+                <p v-if="subtitle" class="subtitle">
+                    {{ subtitle }}
+                </p>
+            </div>
+        </div>
+
+        <div class="hero-foot">
+            <nav class="tabs is-boxed is-fullwidth">
+                <div class="container">
+                    <ul>
+                        <li class="is-active">
+                            <a>Overview</a>
+                        </li>
+                        <li>
+                            <a>Modifiers</a>
+                        </li>
+                        <li>
+                            <a>Grid</a>
+                        </li>
+                        <li>
+                            <a>Elements</a>
+                        </li>
+                        <li>
+                            <a>Components</a>
+                        </li>
+                        <li>
+                            <a>Layout</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </section>
+</template>
+
+<script>
+    import TopNav from './TopNav.vue';
+
+    export default {
+        name: 'Menu',
+        props: {
+            title: String,
+            subtitle: String
+        }, 
+        data() {
+            return {
+                //siteSubTitle: 'A Menu Created with VueJS'
+            };
+        },
+        components: {
+            TopNav
+        }
+    }
+</script>
+
+
---
+++ src/components/TopNav.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="hero-head">
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item is-uppercase has-text-weight-bold">
+                        {{ title }}
+                    </a>
+                    <span class="navbar-burger burger" data-target="navbarMenuHeroB">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </span>
+                </div>
+                <div id="navbarMenuHeroB" class="navbar-menu">
+                    <div class="navbar-end">
+                        <a class="navbar-item is-active">
+                            Home
+                        </a>
+                        <a class="navbar-item">
+                            Examples
+                        </a>
+                        <a class="navbar-item">
+                            Documentation
+                        </a>
+                        <span class="navbar-item">
+                        <a class="button is-info is-inverted">
+                            <span>Download</span>
+                        </a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TopNav',
+    props: [
+        'title',
+    ], 
+    created() {
+        console.log(this.title);
+    }
+}
+</script>
